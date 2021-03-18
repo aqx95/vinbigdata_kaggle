c0ec35c1f335975a7207b5bb290b9ef720f7c824
aqx95 <angqx95@gmail.com>
2021-03-18 13:23:25 +0800

    add load from checkpoint for pretrained weights (densenet)

--- stage2_classification/model.py
+++ stage2_classification/model.py
@@ -14,11 +14,21 @@ class Effnet(nn.Module):
         return x
 
 class Densenet(nn.Module):
-    def __init__(self, model_name, n_class, drop_rate=0.0, pretrained=False):
+    def __init__(self, model_name, n_class, drop_rate=0.0, pretrained=False, checkpoint=False):
       super().__init__()
       self.model = timm.create_model(model_name, drop_rate=drop_rate, pretrained=pretrained)
       n_features = self.model.num_features
-      self.model.fc = nn.Linear(n_features, n_class)
+      self.model.classifier = nn.Linear(n_features, 11)
+      if checkpoint:
+        pretrained_model_path = '/content/drive/Shareddrives/Deep Learning/densenet121_chestx.pth'
+        state_dict = dict()
+        for k, v in torch.load(pretrained_model_path, map_location='cpu')["model"].items():
+            if k[:6] == "model.":
+                k = k.replace("model.", "")
+            state_dict[k] = v
+        self.model.load_state_dict(state_dict)
+      n_features = self.model.num_features
+      self.model.classifier = nn.Linear(n_features, n_class)
 
     def forward(self, x):
         x = self.model(x)
