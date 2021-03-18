89110b18f1c864eb9df7ae29791a6145f89af2ef
aqx95 <angqx95@gmail.com>
2021-03-18 21:10:44 +0800

    dataset class

--- stage2_classification/dataset.py
+++ stage2_classification/dataset.py
@@ -45,21 +45,20 @@ class VinData(Dataset):
 # Augmentations
 def get_train_transforms(config):
     return Compose([
-            RandomResizedCrop(config.image_size, config.image_size),
-            HorizontalFlip(p=0.5),
-            VerticalFlip(p=0.5),
-            Transpose(p=0.5),
-            ShiftScaleRotate(p=0.5),
-            HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.4),
-            RandomBrightnessContrast(brightness_limit=(-0.1,0.1), contrast_limit=(-0.1, 0.1), p=0.4),
+            Resize(config.image_size, config.image_size),
+            HorizontalFlip(p=0.3),
+            VerticalFlip(p=0.3),
+            Transpose(p=0.3),
+            ShiftScaleRotate(p=0.3),
+            HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.3),
+            RandomBrightnessContrast(brightness_limit=(-0.1,0.1), contrast_limit=(-0.1, 0.1), p=0.3),
             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0),
-            Cutout(p=0.5),
             ToTensorV2(p=1.0),
         ], p=1.)
 
 def get_valid_transforms(config):
     return Compose([
-            CenterCrop(config.image_size, config.image_size),
+            Resize(config.image_size, config.image_size),
             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0),
             ToTensorV2(p=1.0),
         ], p=1.)
