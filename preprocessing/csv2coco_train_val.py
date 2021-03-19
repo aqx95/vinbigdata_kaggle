aae24af9be617de1ef0ccf6fd1143376ece6309c
aqx95 <angqx95@gmail.com>
2021-03-19 15:59:28 +0800

    csv2coco conversion for train/val

--- preprocessing/csv2coco_train_val.py
+++ preprocessing/csv2coco_train_val.py
@@ -119,65 +119,69 @@ class Csv2Coco:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='VinBigData_trainval')
     parser.add_argument('--image-size', type=int, required=True, help='image size used for training')
-    parser.add_argument('--fold-num', type=int, required=True, help='training fold number')
+    parser.add_argument('--fold-num', type=int, required=True, help='number of training folds')
     parser.add_argument('--file-type', type=str, required=True, help='image extension name')
-    parser.add_argument('--save-path', type=str, default='data', help='saved path')
+    parser.add_argument('--save-path', type=str, default='datacoco', help='saved path')
     args = parser.parse_args()
 
     print(args)
     #read preprocessed train data
-    csv_file = 'data/final_train.csv'
+    csv_file = '../data/csv/final_train.csv'
     image_dir = ''
-    saved_coco_path = args.save_path
+    saved_coco_path = '../data/' + args.save_path
 
-    total_train_annotations = {}
-    total_val_annotations = {}
     annotations = pd.read_csv(csv_file)
     #rescale to training image size
     annotations[['x_min','x_max']] = annotations[['x_min','x_max']].apply(lambda x:round(x*args.image_size,1))
     annotations[['y_min','y_max']] = annotations[['y_min','y_max']].apply(lambda x:round(x*args.image_size,1))
-    train_annotation = annotations[annotations['fold'] != args.fold_num].values
-    val_annotation = annotations[annotations['fold'] == args.fold_num].values
 
-    for annotation in train_annotation:
-        key = annotation[0].split(os.sep)[-1] #image_id
-        value = np.array([annotation[1:-3]]) #remaining col
-        if key in total_train_annotations.keys():
-            total_train_annotations[key] = np.concatenate((total_train_annotations[key], value), axis=0)
-        else:
-            total_train_annotations[key] = value
-    train_keys = list(total_train_annotations.keys())
+    for fold in range(args.fold_num):
+        print('Fold {}...'.format(fold))
+        total_train_annotations = {}
+        total_val_annotations = {}
+        train_annotation = annotations[annotations['fold'] != fold].values
+        val_annotation = annotations[annotations['fold'] == fold].values
 
-    for annotation in val_annotation:
-        key = annotation[0].split(os.sep)[-1] #image_id
-        value = np.array([annotation[1:-3]]) #remaining col
-        if key in total_val_annotations.keys():
-            total_val_annotations[key] = np.concatenate((total_val_annotations[key], value), axis=0)
-        else:
-            total_val_annotations[key] = value
-    val_keys = list(total_val_annotations.keys())
+        for annotation in train_annotation:
+            key = annotation[0].split(os.sep)[-1] #image_id
+            value = np.array([annotation[1:-3]]) #remaining col
+            if key in total_train_annotations.keys():
+                total_train_annotations[key] = np.concatenate((total_train_annotations[key], value), axis=0)
+            else:
+                total_train_annotations[key] = value
+        train_keys = list(total_train_annotations.keys())
 
-    print("train_n:", len(train_keys), 'val_n:', len(val_keys))
+        for annotation in val_annotation:
+            key = annotation[0].split(os.sep)[-1] #image_id
+            value = np.array([annotation[1:-3]]) #remaining col
+            if key in total_val_annotations.keys():
+                total_val_annotations[key] = np.concatenate((total_val_annotations[key], value), axis=0)
+            else:
+                total_val_annotations[key] = value
+        val_keys = list(total_val_annotations.keys())
 
-    #Create directory
-    if not os.path.exists('%scoco/annotations_%s/'%(saved_coco_path,args.image_size)):
-        os.makedirs('%scoco/annotations_%s/'%(saved_coco_path,args.image_size))
-    # if not os.path.exists('%scoco/images/train2017/'%saved_coco_path):
-    #     os.makedirs('%scoco/images/train2017/'%saved_coco_path)
-    # if not os.path.exists('%scoco/images/val2017/'%saved_coco_path):
-    #     os.makedirs('%scoco/images/val2017/'%saved_coco_path)
+        print("train_n:", len(train_keys), 'val_n:', len(val_keys))
 
-    #Convert CSV to Json
-    print('Converting Trainset...')
-    l2c_train = Csv2Coco(image_dir=image_dir, total_annot=total_train_annotations, arg=args)
-    train_instance = l2c_train.to_coco(train_keys)
-    l2c_train.save_coco_json(train_instance, '%scoco/annotations_%s/instances_train2020.json'%(saved_coco_path, args.image_size))
-    # for file in train_keys:
-    #     shutil.copy(image_dir+file+'.jpg',"%scoco/images/train2020/"%saved_coco_path)
-    # for file in val_keys:
-    #     shutil.copy(image_dir+file+'.jpg',"%scoco/images/val2020/"%saved_coco_path)
-    print('Converting Valid set')
-    l2c_val = Csv2Coco(image_dir=image_dir,total_annot=total_val_annotations, arg=args)
-    val_instance = l2c_val.to_coco(val_keys)
-    l2c_val.save_coco_json(val_instance, '%scoco/annotations_%s/instances_val2020.json'%(saved_coco_path, args.image_size))
+        #Create directory
+        annot_path = os.path.join(saved_coco_path, 'annotation_{}_{}'.format(args.image_size, fold))
+        if not os.path.exists(annot_path):
+            os.makedirs(annot_path)
+        # if not os.path.exists('%scoco/images/train2017/'%saved_coco_path):
+        #     os.makedirs('%scoco/images/train2017/'%saved_coco_path)
+        # if not os.path.exists('%scoco/images/val2017/'%saved_coco_path):
+        #     os.makedirs('%scoco/images/val2017/'%saved_coco_path)
+
+        #Convert CSV to Json
+        print('Converting Trainset...')
+        l2c_train = Csv2Coco(image_dir=image_dir, total_annot=total_train_annotations, arg=args)
+        train_instance = l2c_train.to_coco(train_keys)
+        l2c_train.save_coco_json(train_instance, os.path.join(annot_path,'instances_train2020.json'))
+        # for file in train_keys:
+        #     shutil.copy(image_dir+file+'.jpg',"%scoco/images/train2020/"%saved_coco_path)
+        # for file in val_keys:
+        #     shutil.copy(image_dir+file+'.jpg',"%scoco/images/val2020/"%saved_coco_path)
+        print('Converting Valid set')
+        l2c_val = Csv2Coco(image_dir=image_dir,total_annot=total_val_annotations, arg=args)
+        val_instance = l2c_val.to_coco(val_keys)
+        l2c_val.save_coco_json(val_instance, os.path.join(annot_path, 'instances_val2020.json'))
     print('COCO Conversion Done!')
