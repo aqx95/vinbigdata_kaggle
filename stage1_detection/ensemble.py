f50af7dda2fabca1a624b35f8ada735fd952ac68
aqx95 <angqx95@gmail.com>
2021-05-24 21:35:15 +0800

    script for running inference

--- stage1_detection/ensemble.py
+++ stage1_detection/ensemble.py
@@ -1,120 +1,176 @@
-import zipfile
-import wget
-import glob
 import os
-import torch
+import glob
+import numpy as np
 import argparse
 import pandas as pd
 from tqdm import tqdm
-from config import GlobalConfig
-
-import mmcv
-from mmcv import Config
-from mmdet.apis import set_random_seed
-from mmdet.datasets import build_dataset, build_dataloader
-from mmdet.apis import init_detector, inference_detector, show_result_pyplot
+from ensemble_boxes import *
 
 
-def detector_test(model, config):
-    ## CSV reference
-    test_meta = pd.read_csv(os.path.join(config.csv_path, 'test.csv'))
-    sub = pd.read_csv(os.path.join(config.csv_path, 'sample_submission.csv'))
-    # Loop imag for test
-    for idx, row in tqdm(sub.iterrows(), total=len(sub), position=0, leave=True):
-        img_id = row['image_id']
-        meta = test_meta[test_meta['image_id']==img_id]
-        orig_h, orig_w = meta['height'].item(), meta['width'].item()
-        h_ratio, w_ratio = orig_h/1024, orig_w/1024
+def string_to_row(df, fold_num):
+    csv = df.copy()
+    data = {'image_id':[], 'model':None, 'class':[], 'confidence':[],
+            'x_min':[], 'y_min':[], 'x_max':[], 'y_max': []}
+    img_id = class_ = confidence = x_min = y_mix = x_max = y_max = []
 
-        img = mmcv.imread('../../../test/'+img_id+'.png')
-        result = inference_detector(model, img)
-        string = ""
-        for class_, class_array in enumerate(result):
-            if class_array.shape[0]:
-              class_array = class_array.tolist()
-              for array in class_array:
-                  array[0] = array[0] * w_ratio
-                  array[1] = array[1] * h_ratio
-                  array[2] = array[2] * w_ratio
-                  array[3] = array[3] * h_ratio
-                  string += '{} {:.2f} {} {} {} {} '.format(int(class_), array[4], int(array[0]), int(array[1]), int(array[2]), int(array[3]))
+    for idx, row in tqdm(csv.iterrows(), total=len(csv)):
+        pred = row['PredictionString'].split(' ')
+        for ptr in range(0, len(pred)-5, 6):
+            data['image_id'].append(row['image_id'])
+            data['class'].append(int(pred[ptr]))
+            data['confidence'].append(float(pred[ptr+1]))
+            data['x_min'].append(int(pred[ptr+2]))
+            data['y_min'].append(int(pred[ptr+3]))
+            data['x_max'].append(int(pred[ptr+4]))
+            data['y_max'].append(int(pred[ptr+5]))
 
-        if len(string) == 0:
-            string += "14 1 0 0 1 1"
-
-        sub.loc[idx, 'PredictionString'] = string
-
-    return sub
+    data['model'] = fold_num
+    new_df = pd.DataFrame(data, columns=[key for key in data.keys()])
+    return new_df
 
 
-#Inference
+def row_to_string(df):
+    data = {'image_id':[], 'PredictionString':[]}
+    current = df.iloc[0]['image_id']
+    string = ""
+
+    for idx, row in tqdm(df.iterrows(), total=len(df)):
+        if row.image_id == current:
+            string += f"{row['class']} {row.confidence} {row.x_min} {row.y_min} {row.x_max} {row.y_max} "
+        else:
+            data['image_id'].append(current)
+            data['PredictionString'].append(string)
+            #reset
+            string = f"{row['class']} {row.confidence:.2f} {row.x_min} {row.y_min} {row.x_max} {row.y_max} "
+            current = row.image_id
+    #add last id
+    data['image_id'].append(current)
+    data['PredictionString'].append(string)
+
+    new_df = pd.DataFrame(data, columns=[key for key in data.keys()])
+    return new_df
+
+
+# Weighted Box Fusion
+def postprocess_fusion(df, fusion_type, iou_thr=0.5, sigma=0.1, skip_box_thr=0.0001):
+    results = []
+    image_ids = df["image_id"].unique()
+
+    for image_id in tqdm(image_ids, total=len(image_ids), position=0, leave=True):
+        # All annotations for the current image.
+        data = df[df["image_id"] == image_id]
+        data = data.reset_index(drop=True)
+
+        annotations = {}
+        weights = []
+
+        # WBF expects the coordinates in 0-1 range.
+        max_value = data.iloc[:, 4:].values.max()
+        data.loc[:, ["x_min", "y_min", "x_max", "y_max"]] = data.iloc[:, 4:] / max_value #[4:] denotes x_min,y_min,x_max,y_max
+
+        # Loop through all of the annotations for single image
+        for idx, row in data.iterrows():
+            model_id = row["model"]
+
+            if model_id not in annotations:
+                annotations[model_id] = {
+                    "boxes_list": [],
+                    "scores_list": [],
+                    "labels_list": [],
+                }
+
+                # Assume equal weightage
+                weights.append(1.0)
+
+            annotations[model_id]["boxes_list"].append([row["x_min"], row["y_min"], row["x_max"], row["y_max"]])
+            annotations[model_id]["scores_list"].append(row['confidence'])
+            annotations[model_id]["labels_list"].append(row["class"])
+
+        boxes_list = []
+        scores_list = []
+        labels_list = []
+        #Combine all predcitions from all models for a image_id
+        for annotator in annotations.keys():
+            boxes_list.append(annotations[annotator]["boxes_list"])
+            scores_list.append(annotations[annotator]["scores_list"])
+            labels_list.append(annotations[annotator]["labels_list"])
+
+        # Calculate Fusion
+        if fusion_type == 'wbf':
+            boxes, scores, labels = weighted_boxes_fusion(
+                boxes_list,
+                scores_list,
+                labels_list,
+                weights=weights,
+                iou_thr=iou_thr,
+                skip_box_thr=skip_box_thr)
+
+        if fusion_type == 'nms':
+             boxes, scores, labels = nms(
+                boxes_list,
+                scores_list,
+                labels_list,
+                weights=weights,
+                iou_thr=iou_thr)
+
+        if fusion_type == 'softnms':
+            boxes, scores, labels = soft_nms(
+                boxes_list,
+                scores_list,
+                labels_list,
+                sigma=sigma,
+                weights=weights,
+                iou_thr=iou_thr)
+
+        if fusion_type == 'nmw':
+            boxes, scores, labels = non_maximum_weighted(
+                boxes_list,
+                scores_list,
+                labels_list,
+                weights=weights,
+                iou_thr=iou_thr,
+                skip_box_thr=skip_box_thr)
+
+        #Fused results for a single image
+        for box, score, label in zip(boxes, scores, labels):
+            results.append({
+                "image_id": image_id,
+                "class": int(label),
+                "confidence": round(score, 2),
+                "x_min": int(box[0] * max_value),
+                "y_min": int(box[1] * max_value),
+                "x_max": int(box[2] * max_value),
+                "y_max": int(box[3] * max_value)
+            })
+
+    results = pd.DataFrame(results, columns=['image_id','class','confidence','x_min','y_min','x_max','y_max'])
+    return results
+
+
+
+#Ensemble
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='vinbigdata')
-    parser.add_argument('--checkpoint-path', type=str, required=True,
-                        help='checkpoint path for loading weights')
-    parser.add_argument('--num-epochs', type=int, required=True,
-                        help='number of training epoch')
-    parser.add_argument('--fold-num', type=int, required=True,
-                        help='fold number for training')
+    parser.add_argument('--submission-path', type=str, required=True,
+                        help='csv directory for single model detection')
     args = parser.parse_args()
 
-    os.chdir('mmdetection')
+    #Process each model submission
+    ensemble_df = pd.DataFrame()
+    for idx, subs in enumerate (glob.glob(args.submission_path + '/*.csv')):
+        print('Read from {}'.format(subs.split('/')[-1]))
+        model_df = pd.read_csv(subs)
+        new_model_df = string_to_row(model_df, idx)
+        ensemble_df = pd.concat([ensemble_df, new_model_df], axis=0)
 
-    #overwrite
-    config = GlobalConfig
-    config.num_epochs = args.num_epochs
+    #Do WBF
+    print("Shape of ensembled data before WBF: {}".format(ensemble_df.shape))
+    wbf_ensemble = postprocess_fusion(ensemble_df, fusion_type='wbf')
+    print("Shape of ensembled data after WBF: {}".format(wbf_ensemble.shape))
 
-    logger = open(os.path.join(config.log_path, 'infer_log.txt'), 'a')
+    #Convert back to submission format
+    print("Converting to submission...")
+    final_submission = row_to_string(wbf_ensemble)
+    print("Shape of submission: {}".format(final_submission.shape))
 
-    logger.write("Reading config from: {}\n".format(config.config_file))
-    cfg = Config.fromfile(config.config_file)
-
-    ## Edit configuration settings
-    cfg.classes = ("Aortic enlargement", "Atelectasis", "Calcification", "Cardiomegaly",
-                   "Consolidation", "ILD", "Infiltration", "Lung Opacity", "Nodule/Mass",
-                   "Other lesion", "Pleural effusion", "Pleural thickening", "Pneumothorax", "Pulmonary fibrosis")
-    cfg.data.train.classes = cfg.classes
-    cfg.data.val.classes = cfg.classes
-    cfg.data.test.classes = cfg.classes
-
-    cfg.data_root = '../../../test'
-    cfg.data.train.img_prefix = cfg.data_root
-    cfg.data.val.img_prefix = cfg.data_root
-    cfg.data.test.img_prefix = cfg.data_root
-    cfg.data.train.ann_file = '../../data/datacoco/annotation_1024_{}/instances_train2020.json'.format(args.fold_num)
-    cfg.data.val.ann_file = '../../data/datacoco/annotation_1024_{}/instances_val2020.json'.format(args.fold_num)
-    cfg.data.test.ann_file = '../../data/datacoco/annotation_1024_{}/instances_test2020.json'.format(args.fold_num)
-
-    cfg.model.bbox_head.num_classes = 14
-
-    cfg.data.samples_per_gpu = 4
-    cfg.optimizer.lr = 0.0025
-    cfg.evaluation.interval = 2
-    cfg.checkpoint_config.interval = 2
-    cfg.gpu_ids = range(1)
-    cfg.seed = 0
-    cfg.total_epochs = config.num_epochs
-    cfg.runner.max_epochs = config.num_epochs
-
-    cfg.load_from = config.model_path
-    cfg.work_dir = config.output_path
-
-
-    ## Inference
-    cfg.data.test.test_mode=True
-    cfg.data.test.pipeline[0].type='LoadImageFromFile'
-    cfg.model.test_cfg.score_thr = config.score_threshold
-    config_file = cfg
-    checkpoint_file = args.checkpoint_path
-    logger.write('Read checkpoint at: {}\n'.format(checkpoint_file))
-
-    # build the model from a config file and a checkpoint file
-    model = init_detector(config_file, checkpoint_file, device='cuda:0')
-    model.CLASSES = cfg.classes
-
-    logger.write('Creating submission...\n')
-    submission_file = detector_test(model, config)
-    submission_file.to_csv('submission.csv', index=False)
-    logger.write('Finished!')
-
-    logger.close()
+    final_submission.to_csv(os.path.join(args.submission_path, 'ensemble_submission.csv'), index=False)
